130b29708a1713597ed0562250372d93836a07c6
Nanda Amanta <nansamgr@gmail.com>
2022-01-08 22:02:41 +0800

    add comment in response and validator

--- server/response/response.js
+++ server/response/response.js
@@ -1,5 +1,16 @@
+// =======================================================================================+
+// this class purpose to give response for any input request from client                 |
+// to server , Response explanation :                                                    |
+//                                                                                      |
+//  ok -> this is boolean , to give client info that the request was success or failed  |
+//  statusCode -> this is String, give info to client and server what happend           |
+//  message -> this is String, give info to client what happend / what should do        |
+//  message -> this is Any and optional, give data depend of the req, can be null       |
+// =====================================================================================+
+
 export default class Response {
     
+    // add value data for response using constructor
     constructor(ok, statusCode, message, data) {
         this.ok = ok;
         this.statusCode = statusCode;
@@ -8,6 +19,7 @@ export default class Response {
 
       }
 
+      // convert data response to Json
       toJson(){
           return {
               ok:this.ok,
--- server/util/validator.util.js
+++ server/util/validator.util.js
@@ -1,5 +1,8 @@
 import Response from "../response/response";
 
+// ======================================================================+
+// this file purpose to validate any input request from client to server |
+// ======================================================================+
 
 module.exports ={
 
@@ -7,12 +10,12 @@ module.exports ={
         
         // If Input user name is null
         if(inputUsername  == null || inputUsername.length <= 1){
-            return new Response(true, "USER_NAME_NULL", "Please add your username", null).toJson();
+            return new Response(false, "USER_NAME_NULL", "Please add your username", null).toJson();
         }
 
         // If Input password is null
         if(inputPassword == null || inputPassword.length <= 1){
-            return new Response(true, "PASSWORD_NULL", "Please add your password", null).toJson();
+            return new Response(false, "PASSWORD_NULL", "Please add your password", null).toJson();
         }
     }
 
