beb490b6a2ee290a86934969a41c150d012aeadb
Nanda Amanta <nansamgr@gmail.com>
2022-01-08 23:43:12 +0800

    make generator

---
+++ server/util/generator.util.js
@@ -0,0 +1,24 @@
+module.exports = {
+    generateToken: function (length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJ-KLMNOPQRSTUVWXYZabcdef-ghijklmnopqrstuvwxyz0123456789-';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() *
+                charactersLength));
+        }
+        return result;
+
+    },
+    generateUserId: function () {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < 8; i++) {
+            result += characters.charAt(Math.floor(Math.random() *
+                charactersLength));
+        }
+        return result;
+
+    }
+}
