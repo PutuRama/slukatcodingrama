ce6f2c3c61df8edf214b459e2f435968f86152e9
Nanda Amanta <nansamgr@gmail.com>
2022-01-14 20:26:33 +0800

    what ever

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,19 @@
+
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <topnavbar/>
   <router-view/>
 </template>
 
+<script type="text/javascript">
+  import topnavbar from './components/navbar/TopNavBar.vue';
+
+  export default{
+    components:{
+      topnavbar : topnavbar // register component
+    }
+  }
+
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -13,18 +21,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
