328907a069335dbcdf2b1bf3f234c88981126097
Nanda Amanta <nansamgr@gmail.com>
2022-01-14 20:25:51 +0800

    add topnavbar

---
+++ client/src/components/navbar/TopNavBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="topnavbar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <router-link class="navbar-brand" to="/"><img src="../../assets/logo_slukat.png" alt="Slukat Learning Center"></router-link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <router-link class="nav-link" to="/">Home</router-link>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          About
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <router-link class="dropdown-item" to="/about/slc">SLC</router-link>
+          <router-link class="dropdown-item" to="/about/volounteer">Volounteer</router-link>
+          <router-link class="dropdown-item" to="/about/student">Student</router-link>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Our Programs
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <router-link class="dropdown-item" to="/program/coding">Coding Program</router-link>
+          <router-link class="dropdown-item" to="/program/english">English Program</router-link>
+          <router-link class="dropdown-item" to="/program/enterprenuer">Enterprenuer Program</router-link>
+          <router-link class="dropdown-item" to="/program/agriculture">Agriculture Program</router-link>
+        </div>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/marketplace">Marketplace</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/activities">Activities</router-link>
+      </li>
+    </ul>
+  </div>
+</nav>
+  </div>
+</template>
+<style scoped>
+  .navbar-brand img{
+    max-width: 250px;
+  } 
+</style>
