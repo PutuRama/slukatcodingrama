055f50d5307507bef2aede4a79b46c4fc8d86375
Nanda Amanta <nansamgr@gmail.com>
2022-01-16 13:54:51 +0800

    fix navbar

--- client/src/components/navbar/TopNavBar.vue
+++ client/src/components/navbar/TopNavBar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="topnavbar">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
   <router-link class="navbar-brand" to="/"><img src="../../assets/logo_slukat.png" alt="Slukat Learning Center"></router-link>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    <ul class="navbar-nav ml-auto">
       <li class="nav-item">
         <router-link class="nav-link" to="/">Home</router-link>
       </li>
@@ -46,5 +46,18 @@
 <style scoped>
   .navbar-brand img{
     max-width: 250px;
-  } 
+  }
+  .navbar-nav > li{
+    margin-left:30px;
+    margin-right:30px;
+    transition: 0.5;
+  }
+
+  .nav-link {
+    font-weight: 600;
+    color: rgb(61, 61, 61) !important;
+  }
+  .nav-link:hover {
+    color:green !important;
+  }
 </style>
