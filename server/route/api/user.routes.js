59b8beca471e7f16ccccef893b8722bab4e277e9
Nanda Amanta <nansamgr@gmail.com>
2022-01-20 23:16:15 +0800

    make user controller

---
+++ server/route/api/user.routes.js
@@ -0,0 +1,11 @@
+const { request } = require("express");
+const express = require("express");
+const { route } = require("express/lib/application");
+const userController = require("../../controllers/user.controller");
+const router = express.Router();
+
+router.get("/", (req,res) => userController.getAllUser(req,res));
+router.get("/:userId", (req,res) => userController.getUserByUserId(req,res));
+router.post("/:userId",(req,res) => userController.updateUserByUserId(req,res));
+
+module.exports = router;
