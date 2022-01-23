628ed7215cadaa5fe4b73cb293388d5145cb20ed
I MADE ADI PUTRA SANJAYA <madeputra.6166@gmail.com>
2022-01-23 11:11:19 +0800

    make-our-program

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -37,20 +37,39 @@
       </div>
     </div>
       <br><br>
+
+      <!-- founder -->
       <div class="founder">
       <div class="judul">
         <p>THE FOUNDERS</p>
       </div>
-       <div class="row">
-        <div class="column">
-          <img src="https://asset.kompas.com/crops/skk_fzrMQFRN7DRjYRdyUzTGxDA=/0x14:733x503/750x500/data/photo/2020/09/22/5f69de4196b5e.png" alt="">
+        <div class="row2">
+          <div class="column2">
+            <img src="https://asset.kompas.com/crops/skk_fzrMQFRN7DRjYRdyUzTGxDA=/0x14:733x503/750x500/data/photo/2020/09/22/5f69de4196b5e.png" alt="">
+            <p>I Gusti Agung Rai</p>
+          </div>
+          <div class="column2">
+            <img src="https://asset.kompas.com/crops/skk_fzrMQFRN7DRjYRdyUzTGxDA=/0x14:733x503/750x500/data/photo/2020/09/22/5f69de4196b5e.png" alt="">
+            <p>I Gusti Ayu Darsini</p>
+          </div>
         </div>
-         <div class="column">
-          <img src="https://asset.kompas.com/crops/skk_fzrMQFRN7DRjYRdyUzTGxDA=/0x14:733x503/750x500/data/photo/2020/09/22/5f69de4196b5e.png" alt="">
-        </div>
-      </div>
+        <br>
+        <p>The family of Mr. I Gusti Agung Rai and Mrs. I Gusti Ayu Darsini established SLC on <br> 24 October 2007 to provide a better education opportunity for the children and <br> youth in Keramas Village, in Gianyar, Bali.
+      </p>
+
+        <button class="button-founder"><a href="#">Find Out More</a><i class="fas fa-arrow-circle-right" style="font-size: 30px; color: white; margin-left: 30px;"></i></button>
 
       </div>
+       
+
+
+       <!-- our program -->
+       <div class="our-program">
+         <div class="content">
+           <h2 class="h2">Our Program</h2>
+           <p class="p">Lorem Ipsum is simply dummy text of the <br> printing and typesetting industry. Lorem Ipsum <br> has been the industry's standard dummy text <br> ever since the 1500s, when an unknown printer <br> took a galley of type and scrambled it to make a <br> type specimen book.</p>
+         </div>
+       </div>
 
   </div>
 </template>
@@ -63,7 +82,7 @@
 .img_navbar{
 /* position: relative; */
 width: 100%;
-height: 600px;
+height: 800px;
 }
 .teks{
   position: absolute;
@@ -98,7 +117,7 @@ height: 600px;
   flex-wrap: wrap;
   padding: 0 4px;
   margin-bottom: 20%;
-  top: 105%;
+  top: 80vh;
   position: absolute;
 }
 .column {
@@ -108,7 +127,7 @@ height: 600px;
   margin-left: 35%;
 }
 .column img {
-  margin-top: 20px;
+  /* margin-top: 20px;   */
   width: 30%;
   height: 100%;
   border-top-left-radius: 50px;
@@ -118,10 +137,69 @@ height: 600px;
 }
 
 /* founder */
+.founder{
+  margin-bottom: 5%;
+}
 .judul{
   font-family: 'Montserrat', sans-serif;
   font-family: 'Poppins', sans-serif;
   font-weight: 700;
+  margin-top: 10%;
+}
+.row2 {
+  margin-top: 5%;
+  display: flex;
+  flex-wrap: wrap; 
+  margin-bottom: 4%;
+  justify-content: center;
+}
+
+.column2 img {
+  width: 50%;
+  height: 100%;  
+  vertical-align: middle; 
+  border-radius: 100%;
+}
+.column2 > p {
+  margin-top: 5%;
+ font-weight: 700
+}
+.founder > p { 
+  margin-top: 10px;
+  font-weight: 500;
+}
+.button-founder {
+   background: #047002;
+   border: white 3px solid;
+   border-radius: 5px;
+   padding: 12px 20px;
+   padding: 0.8% 2% 0.8% 2% ;
+}
+.button-founder a {
+  text-decoration: none; 
+  font-size: 18px;
+  color: white;
+  font-weight: 700;
+}
+
+/* our program */
+.our-program{
+  background-color: #F6FEF5;
+  padding: 2% 0 2% 0;
+}
+.content{
+  text-align: left;
+  border-left: 80px solid #A1EE9F;
+}
+.h2, .p {
+  margin-left: 10%;
+}
+.content > h2 {
+  font-weight: bold;
+  color: #047002;
+}
+.content > p { 
+  color: #335B32;
 }
 
 </style>
