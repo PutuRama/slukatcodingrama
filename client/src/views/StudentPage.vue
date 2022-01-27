44897309765e9f6cfea1c2ffc148ffaf69c3d728
Ryuzuky07 <bomartadinata2@gmail.com>
2022-01-27 18:03:37 +0800

    create page student template

--- client/src/views/StudentPage.vue
+++ client/src/views/StudentPage.vue
@@ -12,10 +12,30 @@
       <p>Since its establishment in 2007, more than one thousand students had participated in SLC’s programs.</p>
     </div>
 
-    <div class="containner">
+    <div class="content">
+      <div class="content-left">
+        <h3>Who are they?</h3>
+        <p>Most of them are from Gianyar and they are in the age between 9-18 years old.</p>
+      </div>
+      <div class="content-right">
 
+      </div>
     </div>
 
+    <div class="main">
+      <h2>Why they join SLC?</h2>
+      <div class="main-content">
+        <div class="sub-content">
+
+        </div>
+        <div class="sub-content">
+          
+        </div>
+        <div class="sub-content">
+          
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
