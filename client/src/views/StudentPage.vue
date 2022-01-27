a036ef34a34ca786be8ae149aae72ba1725155d5
Ryuzuky07 <bomartadinata2@gmail.com>
2022-01-27 17:30:12 +0800

    create student page template

---
+++ client/src/views/StudentPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="body">
+    <div class="carousel">
+      <img src="" alt="">
+      <img src="" alt="">
+      <img src="" alt="">
+      <img src="" alt="">
+    </div>
+
+    <div class="title">
+      <h1>Our Students and Alumni</h1>
+      <p>Since its establishment in 2007, more than one thousand students had participated in SLC’s programs.</p>
+    </div>
+
+    <div class="containner">
+
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.body{
+  margin: 0;
+}
+
+.carousel{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.carousel img{
+  width: 170px;
+  height: 353px;
+}
+</style>
