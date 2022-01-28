9cf239be187285e30320e7506d52c41d9a08e70b
I MADE ADI PUTRA SANJAYA <madeputra.6166@gmail.com>
2022-01-28 10:11:22 +0800

    finished-coding-program

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -13,6 +13,11 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutSlc.vue')
   },
   {
+    path: '/program/coding',
+    name: 'coding-program',
+    component: () => import(/* webpackChunkName: "about" */ '../views/coding-program.vue')
+  },
+  {
     path: '/:pathMatch(.*)*',
     name: '404',
     component: () => import(/* webpackChunkName: "about" */ '../views/404.vue')
