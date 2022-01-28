31e0cb8228cfb722e029944df1cd4844f9d48a3b
I MADE ADI PUTRA SANJAYA <madeputra.6166@gmail.com>
2022-01-28 10:27:39 +0800

    finisihed agriculture

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -13,6 +13,11 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutSlc.vue')
   },
   {
+    path: '/program/agriculture',
+    name: 'AgricultureProgram',
+    component: () => import(/* webpackChunkName: "about" */ '../views/agriculture-program.vue')
+  },
+  {
     path: '/:pathMatch(.*)*',
     name: '404',
     component: () => import(/* webpackChunkName: "about" */ '../views/404.vue')
