fcd5b749794fbac10c0a880c575a94a84a46d151
I MADE ADI PUTRA SANJAYA <madeputra.6166@gmail.com>
2022-01-31 14:22:06 +0800

    .

--- client/src/views/activities.vue
+++ client/src/views/activities.vue
@@ -1,211 +1,224 @@
 <template>
     <div class="activities">
+      
         <div class="activities-carousel">
             <span class="activities-text">
                 <h1>All Our Activities</h1>
             </span>
         </div>
-
-
-    <div class="daily">
-      <div class="row">
-        <div class="col daily-text">
-          <span class="daily-text-span">
-            <h1>What We Do?</h1>
-            <p>
-              The creation of rural youth with character and environmental insight who are able to compete in the global job market and education and are trusted to become leaders.
-            </p>
-          </span>
-        </div>
-        <div class="col daily-img">
-        </div>
-      </div>
-    </div>
-
-
-        <div class="activity">
-      <h1>Our Activities</h1>
-
-      <div class="activity-dekstop">
-        <div class="program">
-            <div class="row">
-              <div class="col-5">
-                <span>
-                  <img src="../assets/dummyteam.png" alt=""><br>
-                </span>
-              </div>
-              <div class="col-2">
-                <h3>ENGLISH CLASS</h3>
-              </div>
-              <div class="col-5">
-                <img src="../assets/dummyteam.png" alt=""><br>
-              </div>
-          </div>
-        </div>
-        <div class="director">
-            <div class="row">
-              <div class="col-3">
-                <span>
-                  <h3>CODING PROGRAM</h3>
-                  <img src="../assets/dummyteam.png" alt=""><br>
-                 
-                </span>
-              </div>
-              <div class="col-6">
-                <span>
-                  <h3>ENTERPRENUER PROGRAM</h3>
-                  <img src="../assets/dummyteam.png" alt=""><br>
-                </span>
-              </div>
-              <div class="col-3">
-                <span>
-                  <h3>AGRICULTURE PROGRAM</h3>
-                  <img src="../assets/dummyteam.png" alt=""><br>
-                </span>
-              </div>
-          </div>
-        </div>
-
         
+
+         <div class="how">
+      <div class="row row-how">
+        <div class="col-lg-6 col-md-6 col-sm-12 column-how">
+          <h2>What We Do?</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 column-how">
+          <h2>What Is The Impact?</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
       </div>
+    </div>
 
-      <div class="activity-mobile">
-        <div class="program">
-            <div class="row">
-              <div class="col-12">
-                <h3>ENGLISH CLASS</h3>
-              </div>
-              <div class="col-12">
-                <span>
-                  <img src="../assets/dummyteam.png" alt=""><br>
-                </span>
-              </div>
+
+    <div class="img-activities">
+      <div class="row row-img">
+        <div class="col-md-6 column-img">
+        <a href="">
+          <img src="../assets/post.jpg" alt="">
+          <div class="img-text">
+            <p>OUR PROGRAM</p> <span> 02 June 2022</span>
+            <h3>lorem ipsum dolor sit jamet</h3>
           </div>
-        </div>
-        <div class="director">
-            <div class="row">
-              <div class="col-12">
-                <span>
-                  <div><h3>CODING PROGRAM</h3></div>
-                  <img src="../assets/dummyteam.png" alt=""><br>
-                </span>
-              </div>
-              <div class="col-12">
-                <span>
-                  <div><h3>ENTERPRENUER PROGRAM</h3></div>
-                  <img src="../assets/dummyteam.png" alt=""><br>
-                  
-                </span>
-              </div>
-              <div class="col-12">
-                <span>
-                  <div><h3>AGRICULTURE PROGRAM</h3></div>
-                  <img src="../assets/dummyteam.png" alt=""><br>
-                </span>
-              </div>
-          </div>
+        </a>
         </div>
 
+       <div class="col-md-6 column-img">
+        <a href="">
+          <img src="../assets/post.jpg" alt="">
+          <div class="img-text">
+            <p>ABOUT STUDENT</p><span> 02 June 2022</span>
+            <h3>lorem ipsum dolor sit jamet</h3>
+          </div>
+        </a>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="list-activities">
+      <div class="list-text">
+        <br><br><br><br>
+        <h2 style="margin-bottom:5%">Our Daily Activities</h2>
+      </div>
+        <div class="col daily-text">
+          <h4 style="text-align: left; padding-left: 10%"><b>English Class</b> </h4>
+        </div>
+      <div class="row row-daily">
     
+        <div class="col-md-4 column-daily">
+        <a href="">  
+         <img src="../assets/post.jpg" alt="">
+          </a> 
+          <div class="daily-text">
+            <p>ABOUT STUDENT</p><span> 02 June 2022</span>
+            <h5><a href="">lorem ipsum dolor sit jame lorem ipsum dolor sit jamett</a></h5>
+          </div>
+        </div>
+        <div class="col-md-4 column-daily">
+         <a href="">
+  
+         <img src="../assets/post.jpg" alt="">
+           </a> 
+           <div class="daily-text">
+            <p>ABOUT STUDENT</p><span> 02 June 2022</span>
+            <h5><a href="">lorem ipsum dolor sit jame lorem ipsum dolor sit jamett</a></h5>
+          </div>
+        </div>
+        <div class="col-md-4 column-daily">
+         <a href="">
+  
+         <img src="../assets/post.jpg" alt="">
+           </a> 
+           <div class="daily-text">
+            <p>ABOUT STUDENT</p><span> 02 June 2022</span>
+           <h5><a href="">lorem ipsum dolor sit jame lorem ipsum dolor sit jamett</a></h5>
+          </div>
+        </div>
       </div>
-      
     </div>
+    <br><br><br>
 
-        <div class="blog">
-      <div class="blog-text">
-        <h5>OUR DAILY ACTIVITIES</h5>
-      </div>
-      <div class="blog-picture">
-        <div class="blog-icon">
-          <img
-            src="https://asset.kompas.com/crops/skk_fzrMQFRN7DRjYRdyUzTGxDA=/0x14:733x503/750x500/data/photo/2020/09/22/5f69de4196b5e.png"
-            alt=""
-          />
-          <h5>
-            English Class | <span class="blog-span">18/01/2022</span>
-          </h5>
-          <p class="desc">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
-          <button class="button-blog">
-            <a href="#">Read More</a
-            ><i
-              class="fas fa-arrow-circle-right"
-              style="font-size: 30px; color: white; margin-left: 30px"
-            ></i>
-          </button>
+    <div class="list-activities">
+        <div class="col daily-text">
+          <h4 style="text-align: left; padding-left: 10%"><b>Coding Program</b> </h4>
         </div>
-        <div class="blog-icon">
-          <img
-            src="https://asset.kompas.com/crops/skk_fzrMQFRN7DRjYRdyUzTGxDA=/0x14:733x503/750x500/data/photo/2020/09/22/5f69de4196b5e.png"
-            alt=""
-          />
-          <h5>
-            Coding Program | <span class="blog-span">18/01/2022</span>
-          </h5>
-          <p class="desc">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
-          <button class="button-blog">
-            <a href="#">Read More</a
-            ><i
-              class="fas fa-arrow-circle-right"
-              style="font-size: 30px; color: white; margin-left: 30px"
-            ></i>
-          </button>
+      <div class="row row-daily">
+    
+        <div class="col-md-4 column-daily">
+        <a href="">  
+         <img src="../assets/post.jpg" alt="">
+          </a> 
+          <div class="daily-text">
+            <p>ABOUT STUDENT</p><span> 02 June 2022</span>
+            <h5><a href="">lorem ipsum dolor sit jame lorem ipsum dolor sit jamett</a></h5>
+            </div>
         </div>
-        <div class="blog-icon">
-          <img
-            src="https://asset.kompas.com/crops/skk_fzrMQFRN7DRjYRdyUzTGxDA=/0x14:733x503/750x500/data/photo/2020/09/22/5f69de4196b5e.png"
-            alt=""
-          />
-          <h5>
-            Enterpreneur Program | <span class="blog-span">18/01/2022</span>
-          </h5>
-          <p class="desc">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
-          <button class="button-blog">
-            <a href="#">Read More</a
-            ><i
-              class="fas fa-arrow-circle-right"
-              style="font-size: 30px; color: white; margin-left: 30px"
-            ></i>
-          </button>
+        <div class="col-md-4 column-daily">
+         <a href="">
+  
+         <img src="../assets/post.jpg" alt="">
+           </a> 
+           <div class="daily-text">
+            <p>ABOUT STUDENT</p><span> 02 June 2022</span>
+            <h5><a href="">lorem ipsum dolor sit jame lorem ipsum dolor sit jamett</a></h5>
+          </div>
         </div>
-        <div class="blog-icon">
-          <img
-            src="https://asset.kompas.com/crops/skk_fzrMQFRN7DRjYRdyUzTGxDA=/0x14:733x503/750x500/data/photo/2020/09/22/5f69de4196b5e.png"
-            alt=""
-          />
-          <h5>
-            Agriculture Program | <span class="blog-span">18/01/2022</span>
-          </h5>
-          <p class="desc">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
-          </p>
-          <button class="button-blog">
-            <a href="#">Read More</a
-            ><i
-              class="fas fa-arrow-circle-right"
-              style="font-size: 30px; color: white; margin-left: 30px"
-            ></i>
-          </button>
+        <div class="col-md-4 column-daily">
+         <a href="">
+  
+         <img src="../assets/post.jpg" alt="">
+           </a> 
+           <div class="daily-text">
+            <p>ABOUT STUDENT</p><span> 02 June 2022</span>
+            <h5><a href="">lorem ipsum dolor sit jame lorem ipsum dolor sit jamett</a></h5>
+          </div>
         </div>
       </div>
     </div>
+    <br><br><br>
+
+
+        <div class="list-activities">
+        <div class="col daily-text">
+          <h4 style="text-align: left; padding-left: 10%"><b>Enterpreneur Program</b> </h4>
+        </div>
+      <div class="row row-daily">
+    
+        <div class="col-md-4 column-daily">
+        <a href="">  
+         <img src="../assets/post.jpg" alt="">
+          </a> 
+          <div class="daily-text">
+            <p>ABOUT STUDENT</p><span> 02 June 2022</span>
+            <h5><a href="">lorem ipsum dolor sit jame lorem ipsum dolor sit jamett</a></h5>
+          </div>
+        </div>
+        <div class="col-md-4 column-daily">
+         <a href="">
+  
+         <img src="../assets/post.jpg" alt="">
+           </a> 
+           <div class="daily-text">
+            <p>ABOUT STUDENT</p><span> 02 June 2022</span>
+            <h5><a href="">lorem ipsum dolor sit jame lorem ipsum dolor sit jamett</a></h5>
+          </div>
+        </div>
+        <div class="col-md-4 column-daily">
+         <a href="">
+  
+         <img src="../assets/post.jpg" alt="">
+           </a> 
+           <div class="daily-text">
+            <p>ABOUT STUDENT</p><span> 02 June 2022</span>
+            <h5><a href="">lorem ipsum dolor sit jame lorem ipsum dolor sit jamett</a></h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br><br><br>
+    
+
+
+    <div class="list-activities">
+        <div class="col daily-text">
+          <h4 style="text-align: left; padding-left: 10%"><b>Agriculture Program</b> </h4>
+        </div>
+      <div class="row row-daily">
+    
+        <div class="col-md-4 column-daily">
+        <a href="">  
+         <img src="../assets/post.jpg" alt="">
+          </a> 
+          <div class="daily-text">
+            <p>ABOUT STUDENT</p><span> 02 June 2022</span>
+            <h5><a href="">lorem ipsum dolor sit jame lorem ipsum dolor sit jamett</a></h5>
+          </div>
+        </div>
+        <div class="col-md-4 column-daily">
+         <a href="">
+  
+         <img src="../assets/post.jpg" alt="">
+           </a> 
+           <div class="daily-text">
+            <p>ABOUT STUDENT</p><span> 02 June 2022</span>
+            <h5><a href="">lorem ipsum dolor sit jame lorem ipsum dolor sit jamett</a></h5>
+          </div>
+        </div>
+        <div class="col-md-4 column-daily">
+         <a href="">
+  
+         <img src="../assets/post.jpg" alt="">
+           </a> 
+           <div class="daily-text">
+            <p>ABOUT STUDENT</p><span> 02 June 2022</span>
+           <h5><a href="">lorem ipsum dolor sit jame lorem ipsum dolor sit jamett</a></h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br><br><br>
 
     </div>
 </template>
@@ -217,7 +230,7 @@
 }
 .activities-carousel{
     background-image: url("../assets/banner_about_us.png");
-    min-height: 500px;
+    min-height: 600px;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -227,198 +240,115 @@
     color: white;
 }
 
-.daily-img{
-  background-image: url("../assets/vision_dummy.png");
-}
-.daily-text{
-  padding: 30px 20px;
-  background-color: #F6FEF5;
-
-}
-.daily-text-span{
-  text-align: start;
-  display: inline-block;
-  max-width: 500px;
-  color:  #047002;
-}
-.daily-text-span h1{
-  font-weight: 700;
-}
 
 
 
-.activity > h1{
-  margin-bottom:100px;
-  font-weight: 700;
-}
-.activity{
-    margin-top: 5%;
-}
-.activity h3{
-  background-color: #A1EE9F;
-  display: inline-block;
-  font-weight: 700;
-}
-
-.activity-dekstop .program .col-5:first-child{
-  text-align: end;
-}
-
-.activity-dekstop .program .col-5:last-child{
-  text-align: start;
-}
-
-.activity-dekstop .program img{
-  width:184px;
-  height:184px;
-  border-radius: 100%;
-  margin-bottom: 20px;
-}
-
-.program , .director{
-  margin-bottom: 100px;
-}
-
-.activity-dekstop span{
-  text-align: center;
-  position: relative;
-  display: inline-block;
-  width:184px;
-}
-
-.activity-dekstop h5{
-  display: inline-block;
-
-}
 
 
-.activity-dekstop .director .col-3:first-child{
-  text-align: end;
-}
 
-.activity-dekstop .director .col-3:last-child{
-  text-align: start;
-}
-
-.activity-dekstop .director .col-3 img{
-  width:131px;
-  height:131px;
-  border-radius: 100%;
-  margin-bottom: 20px;
-}
-
-
-.activity-dekstop .secretary .col-4:first-child{
-  text-align: end;
-}
-
-.activity-dekstop .secretary .col-4:last-child{
-  text-align: start;
-}
-
-.activity-dekstop .secretary .col-4 img{
-  width:131px;
-  height:131px;
-  border-radius: 100%;
-  margin-bottom: 20px;
-}
-
-/* .activity-dekstop{
-  display: none;
-} */
-
-
-.activity-mobile{
-    display: none;
-}
-
-.activity-mobile .col-12 {
-  margin-bottom: 40px;
-}
-.activity-mobile .col-12 img{
-  margin-bottom: 20px;
-}
-.activity{
-  margin-bottom: 100px;
-}
-
-@media only screen and (max-width: 745px) {
-  .activity-mobile{
-    display: block;
-  }
-  .activity-dekstop{
-    display: none;
+@media screen and (max-width: 750px) {
+  .column-how {
+    margin: 30px 0;
   }
 }
-
-.blog {
-  background-color: #f6fef5;
-  padding: 3% 0 3% 0;
-  margin-bottom: 5%;
+.how {
+  margin-top: 10%;
 }
-.blog-text h5 {
-  font-weight: 900;
-  letter-spacing: 0.2em;
-  color: #0f700d;
-}
-.blog-picture {
-  max-height: 100%;
+.row-how {
   display: flex;
-  margin-top: 2%;
-  margin-left: 3%;
-  margin-right: 2%;
-  overflow-x: auto;
-}
-.blog-picture::-webkit-scrollbar {
-  width: 0;
-}
-.blog-picture .blog-icon {
-  min-width: 468px;
-  border-radius: 10px;
-  height: 485px;
-  text-align: center;
-  background-color: #047d02;
-  margin-right: 7%;
-  padding: 0 20px 0 20px;
-  justify-items: center;
-}
-.blog-picture .blog-icon > img {
-  width: 100%;
-  height: 10em;
-  margin-top: 5%;
   justify-content: center;
 }
-.blog-picture .blog-icon p {
-  color: white;
-  text-align: left;
+.column-how {
+  padding-left: 10%;
+  padding-right: 10%;
 }
-.blog-picture .blog-icon h5 {
-  color: white;
-  text-align: left;
-  font-size: 16px;
+.column-how h2 {
+  font-weight: bold;
+}
+.column-img img{
+  min-height: 400px;
+  width: 600px;
   margin-top: 10%;
-  font-weight: 900;
-}
-.blog-picture .blog-icon .blog-span {
-  font-size: 13px;
-  color: #52b054;
-}
-.desc {
-  text-align: center;
+  justify-content: center;
+  transition: .5s ease;
+
+} 
+.column-img img:hover{
+  opacity: 0.8;
 }
 
-.button-blog {
-  background: #047002;
-  border: white 1px solid;
-  padding: 12px 20px;
-  padding: 0.8% 2% 0.8% 2%;
-}
-.button-blog a {
+.img-text{
   text-decoration: none;
-  font-size: 18px;
+  text-align: start;
+  position: absolute;
+  bottom: 0;
   color: white;
-  vertical-align: top;
-  padding: 2em 0 0 1em;
-  font-weight: 700;
+  padding: 20px 60px;
+}
+.img-text h3{
+  font-weight: bold;
+}
+.img-text{
+   opacity: 0.8;
+}
+.img-text p{
+  background-color: #A1EE9F;
+  padding: 0 10px 0 10px;
+  text-transform: uppercase;
+  display: inline;
+  font-weight: bold;
+}
+.img-text span{
+  margin-left: 8%;
+}
+
+
+
+
+
+.column-daily{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  
+}
+.row-daily{
+ flex: 50px;
+ margin: 0 10% 0 10%;
+}
+.column-daily img{
+  width: 100%;
+  margin-bottom: 3%;
+  transition: .5s ease;
+}
+.column-daily img:hover{
+  opacity: 0.8;
+}
+.daily-text{
+  text-decoration: none;
+  text-align: left;
+  margin-right: 10%;
+
+}
+.daily-text p{
+  background-color: #A1EE9F;
+  padding: 5px 10px 5px 10px;
+  color: white;
+  border-radius: 3px;
+  font-size: 15px;
+  text-transform: uppercase;
+  display: inline;
+  font-weight: bold;
+}
+.daily-text h5 a{
+  color: black;
+}
+.daily-text h5{
+  margin-top: 5%;
+}
+.daily-text span{
+  font-size: 13px;
+  text-transform: uppercase;
+  margin-left: 5%;
 }
 </style>
